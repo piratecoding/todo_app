57de2c1d48d3762de712723424f8567216a9a08e
rails -v <amaratc@gmail.com>
2019-01-06 21:04:56 +0100

    Inital commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <React.fragment>
+            <h1>About</h1>
+            <p>This is TodoList App v1.0.0. It serves for learning purposes.</p>
+        </React.fragment>
+    )
+}
+
+export default About;
